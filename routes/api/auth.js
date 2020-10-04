0a9f982858bac91a0e9496d8b8539fa89e50912b
mdsheraj <mdsheraj123@gmail.com>
2020-10-04 18:22:22 +0530

    admin login is successful now

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -29,40 +29,86 @@ const Account = require("../../models/Account");
 
 router.post("/register", (req, res) => { //use password sheraj123
     Account.findOne({ username: req.body.username })
-      .then(account => {
+    .then(account => {
         if (account) {
-          return res
+            return res
             .status(400)
             .json({ accounterror: "username is already registered in our system" });
         } else if(req.body.password!="sheraj123") {
             return res
-              .status(400)
-              .json({ accounterror: "use the predefined password to create an Admin account" });
+                .status(400)
+                .json({ accounterror: "use the predefined password to create an Admin account" });
         } else {
-          const newAccount = new Account({
+            const newAccount = new Account({
             username: req.body.username,
             password: "sheraj123",
             access: "Admin"
-          });
-          //Encrypt password using bcrypt
-          bcrypt.genSalt(10, (err, salt) => {
+            });
+            //Encrypt password using bcrypt
+            bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(newAccount.password, salt, (err, hash) => {
-              if (err) throw err;
-              newAccount.password = hash;
-              newAccount
+                if (err) throw err;
+                newAccount.password = hash;
+                newAccount
                 .save()
                 .then(account => res.json(account))
                 .catch(err => console.log(err));
             });
-          });
+            });
         }
-      })
-      .catch(err => console.log(err));
-  });
+    })
+    .catch(err => console.log(err));
+});
   
 
 
 
+// @type    POST
+// @route   /api/auth/login
+// @desc    route for login of users
+// @access  PUBLIC
+
+router.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    Account.findOne({ username })
+    .then(account => {
+    if (!account) {
+        return res
+        .status(404)
+        .json({ accounterror: "User not found with this username" });
+    }
+    bcrypt
+        .compare(password, account.password)
+        .then(isCorrect => {
+        if (isCorrect) {
+            // res.json({ success: "User is able to login successfully" });
+            //use payload and create token for user
+            const payload = {
+                id: account.id,
+                username: account.username
+            };
+            jsonwt.sign(
+                payload,
+                key.secret,
+                { expiresIn: 5*60*60 },
+                (err, token) => {
+                    res.json({
+                    success: true,
+                    token: "Bearer " + token
+                    });
+                }
+            );
+        } else {
+            res.status(400).json({ passworderror: "Password is not correct" });
+        }
+        })
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+});
+  
 
 
 
