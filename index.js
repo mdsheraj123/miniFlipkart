c2baf7e4b7a6ae96ffd640ffecc8c997b1bbc1f2
mdsheraj <mdsheraj123@gmail.com>
2020-10-04 17:32:21 +0530

    mongoose connected

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
 //https://stackoverflow.com/questions/33375726/manage-authentication-of-different-types-of-users-in-express-js-using-passport-j
 //Set a role string for different login types
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+
+//bring all routes
+const auth = require("./routes/api/auth");
+
+const app = express();
+
+
+//Middleware for bodyparser
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+//mongoDB configuration
+const db = require("./setup/myurl").mongoURL;
+
+//Attempt to connect to database
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch(err => console.log(err));
+
+
+
+//just for testing  -> route
+app.get("/", (req, res) => {
+    res.send("Server is running, please use Postman");
+});
+
+
+//actual routes
+app.use("/api/auth", auth);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`App is running at ${port}`));
