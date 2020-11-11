2c2827dc3a670d560a8b144837f3e1c59cbd4811
Mohammad Sheraj <mdsheraj123@gmail.com>
2020-11-12 01:08:24 +0530

    Update myurl.js

--- setup/myurl.js
+++ setup/myurl.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURL: "mongodb+srv://mdsheraj:162676w.M@cluster0.l5sqi.mongodb.net/Cluster0?retryWrites=true&w=majority",
-  secret: "itisabigbigworld"
+  mongoURL: "",
+  secret: ""
 };
